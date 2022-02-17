f91ff208094e377577b57bed84d32c5c55d70299
emilssats <emilio268xoxo@gmail.com>
2022-02-17 14:50:01 +0200

    Create scirpt.js

---
+++ scirpt.js
@@ -0,0 +1,12 @@
+function nolasa (m1, m2, m3){
+    const malas = document.getElementsByClassName("mala");
+    const m1 = parseFLoat(mala1.value);
+    
+    if (m1 > 0 && m2 > 0 && m3 > 0){
+        console.log({m1, m2, m3});
+        return {m1, m2, m3};
+    } else {
+        console.log(false);
+    return false;
+    }
+}
