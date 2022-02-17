dabec343fb8f2b185358106a1d0cec93a0b85616
emilssats <emilio268xoxo@gmail.com>
2022-02-17 14:51:23 +0200

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -1,7 +1,10 @@
 function nolasa (m1, m2, m3){
     const malas = document.getElementsByClassName("mala");
     const m1 = parseFLoat(mala1.value);
+    const m2 = parseFLoat(mala2.value);
+    const m3 = parseFLoat(mala3.value);
     
+
     if (m1 > 0 && m2 > 0 && m3 > 0){
         console.log({m1, m2, m3});
         return {m1, m2, m3};
